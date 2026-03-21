4fe35965e9bf2d57ab4cc2a7ffd55488c02ac78f
Bruno Silva Marques <10889019+brunobola@users.noreply.github.com>
2026-03-21 20:46:21 +0000

    feat(ui): register MigrationWizard modal and remap 'i' shortcut

--- src/hooks/useKeyboardShortcuts.js
+++ src/hooks/useKeyboardShortcuts.js
@@ -14,6 +14,7 @@ export function useKeyboardShortcuts({
     onSearch,
     onCreateRepo,
     onImport,
+    onMigrate,
     onViewChange,
     enabled = true
 }) {
@@ -48,7 +49,7 @@ export function useKeyboardShortcuts({
                 break
             case 'i':
                 e.preventDefault()
-                onImport?.()
+                onMigrate?.()
                 break
             case 'd':
                 e.preventDefault()
@@ -73,7 +74,7 @@ export function useKeyboardShortcuts({
                 }
                 break
         }
-    }, [enabled, onSearch, onCreateRepo, onImport, onViewChange, showHelp])
+    }, [enabled, onSearch, onCreateRepo, onMigrate, onViewChange, showHelp])
 
     useEffect(() => {
         document.addEventListener('keydown', handleKeyDown)
